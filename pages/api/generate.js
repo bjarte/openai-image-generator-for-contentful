d7899c4bf876d2e71807d5f42514fbd286ffebea
Bjarte Aune Olsen <bjarte@auneolsen.no>
2022-12-04 10:16:36 +0100

    Debugging azure app by exposing api key

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 const openai = new OpenAIApi(new Configuration({ apiKey: process.env.REACT_APP_OPENAI_API_KEY }));
 
-export default async function generatorApi (req, res) {
+export default async function generatorApi(req, res) {
 
   const image = await openai.createImage({
     prompt: req.body.description,
@@ -10,12 +10,16 @@ export default async function generatorApi (req, res) {
     size: "1024x1024",
   });
 
+  // res.status(200).json({
+  //   result: [
+  //     image.data.data[0].url,
+  //     image.data.data[1].url,
+  //     image.data.data[2].url,
+  //     image.data.data[3].url
+  //   ]
+  // });
+
   res.status(200).json({
-    result: [
-      image.data.data[0].url,
-      image.data.data[1].url,
-      image.data.data[2].url,
-      image.data.data[3].url
-    ]
+    result: [process.env.REACT_APP_OPENAI_API_KEY]
   });
 }
